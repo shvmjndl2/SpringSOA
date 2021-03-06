7c5e9656e5ca7ef9a102d215e92e2a052e099844
shvmjndl2 <shvmjndl2@gmail.com>
2021-03-06 15:13:41 +0530

    Create RecentlyCreatedRecordsHelper.js

---
+++ RecentlyCreatedRecordsHelper.js
@@ -0,0 +1,21 @@
+({
+    fetchAccHelper : function(component, event, helper) {
+        //we can use wrapper class to get rows and col dynamically
+        component.set('v.mycolumns', [
+            {label: 'Account Name', fieldName: 'Name', type: 'text'},
+                {label: 'Industry', fieldName: 'Industry', type: 'text'},
+                {label: 'Phone', fieldName: 'Phone', type: 'Phone'},
+                {label: 'Website', fieldName: 'Website', type: 'url '}
+            ]);
+        var action = component.get("c.fetchAccounts");
+        action.setParams({
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.acctList", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
